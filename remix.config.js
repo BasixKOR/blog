4855df60629a44334cfd81f5c05f337fd38659be
Basix <basix@basix.tech>
2021-11-25 00:41:56 +0900

    Use custom routing for blog posts

--- remix.config.js
+++ remix.config.js
@@ -1,3 +1,6 @@
+const { readdir } = require("fs/promises");
+const DEFAULT_LANG = "en";
+
 /**
  * @type {import('@remix-run/dev/config').AppConfig}
  */
@@ -6,5 +9,23 @@ module.exports = {
   browserBuildDirectory: "public/build",
   publicPath: "/build/",
   serverBuildDirectory: "build",
-  devServerPort: 8002
+  devServerPort: 8002,
+  async routes(defineRoutes) {
+    const contents = await readdir("src/posts");
+    const posts = contents.filter((c) => /\.mdx?$/.test(c));
+
+    const fileRegex = /^(?<slug>[^\.]+)(?:\.(?<lang>\w+))?\.mdx?$/;
+
+    return defineRoutes((route) => {
+      for (const post of posts) {
+        const match = fileRegex.exec(post);
+        const { lang = DEFAULT_LANG, slug } = match?.groups;
+        if (slug) {
+          route(`/blog/${lang}/${slug}`, `../posts/${post}`);
+          if (lang === DEFAULT_LANG)
+            route(`/blog/${slug}`, `../posts/${post}`);
+        }
+      }
+    });
+  },
 };
